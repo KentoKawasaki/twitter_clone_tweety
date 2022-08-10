e9ca2a0202736e608b5b8694de7deca783005237
KentoKawasaki <projectdivakkmd@gmail.com>
2022-08-10 11:29:05 +0900

    Added searchUserInMsg.php. Modified search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -5,4 +5,13 @@ $(function(){
             $('.search-result').html(data);
         })
     }));
+
+    $(document).on('keyup', '.search-user', function(){
+        $('.message-recent').hide();
+        let search = $(this).val();
+
+        $.post(baseURL + 'core/ajax/searchUserInMsg.php', {search:search}, function(data){
+            $('.message-body').html(data);
+        });
+    });
 });
